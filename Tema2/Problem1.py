2cf29eabeaa8a7296426b84c07d4a813e1b211e1
utilizatorvalid <vasilegroza3@gmail.com>
2017-03-06 00:33:53 +0200

    Starting GUI

--- Tema2/Problem1.py
+++ Tema2/Problem1.py
@@ -1,3 +1,5 @@
+import numpy as np
+from tkinter import *
 f = open("input.txt")
 A = [[float(x) for x in line.split()] for line in f]
 b = [1, 2, 3]
@@ -33,7 +35,39 @@ for j in range(n-1, -1, -1):
     x[j] = y[j] - sum([A[i][j] * x[i] for i in range(j, n)])
 
 
-print(z)
-print(x)
-print(d)
+
+print('solutia este:' ,x)
 print(A)
+
+for i in range(n):
+    for j in range(i+1, n):
+        A[j][i]= A[i][j]
+print(A)
+print("*************************")
+L = np.linalg.cholesky(np.array(A))
+print(L)
+print('Solutia aplicand algoritmul din libraria numpy este:')
+print(np.linalg.solve(A,b))
+print("*************************")
+distances = np.subtract(np.dot(A,x),b)
+
+print(sum([abs(d) for d in distances]))
+
+def solve(t:Text):
+    print("work starts...")
+    print(t.get("1.0",END))
+    # print(t.get())
+    pass
+
+root = Tk()
+
+root.wm_title("Tema2")
+root.geometry('{}x{}'.format(400, 400))
+
+t = Text(root, height=5, width=20)
+Button(root, text="Apasa", command = lambda : solve(t)).grid()
+v = StringVar()
+l = Label(root, text="Matrix A")
+l.grid(row=1,column=0)
+t.grid(padx = 10)
+root.mainloop()
